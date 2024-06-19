e10a424ee9d5ce99afa90fd50a3c3a41a5d8644e
Aaron Mai <aaronmai@Aarons-Laptop.local>
2024-06-19 14:48:04 -0400

    Now loads all list data including table headers from JSON

--- app.vue
+++ app.vue
@@ -40,6 +40,7 @@ export default {
 
           <thead class="bg-gray-50">
             <tr>
+              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ itemData.item_type }}</th>
               <th v-for="check in itemData.check_items" :key="check" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ check }}</th>
             </tr>
           </thead>
@@ -50,16 +51,8 @@ export default {
 
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ item }}</td>
 
-              <td class="px-6 py-4 whitespace-nowrap">
-                <input type="checkbox" :id="`${item}-checkbox1`" class="form-checkbox h-5 w-5 text-blue-600" />
-              </td>
-
-              <td class="px-6 py-4 whitespace-nowrap">
-                <input type="checkbox" :id="`${item}-checkbox2`" class="form-checkbox h-5 w-5 text-blue-600" />
-              </td>
-
-              <td class="px-6 py-4 whitespace-nowrap">
-                <input type="checkbox" :id="`${item}-checkbox3`" class="form-checkbox h-5 w-5 text-blue-600" />
+              <td v-for="check in itemData.check_items" :key="check" class="px-6 py-4 whitespace-nowrap">
+                <input type="checkbox" :id="`${item}-${check}-checkbox`" class="form-checkbox h-5 w-5 text-blue-600" />
               </td>
 
             </tr>
@@ -68,7 +61,7 @@ export default {
 
         <div class="mt-4">
           <label for="notes" class="block text-sm font-medium text-gray-700">Notes</label>
-          <textarea id="notes" name="notes" rows="4" class="mt-1 block w-full rounded-md border-black shadow-md focus:border-blue-500 focus:ring-blue-500 sm:text-sm"></textarea>
+          <textarea id="notes" name="notes" rows="4" class="pl-2 pt-1 mt-1 block w-full rounded-md border-black shadow-md focus:border-blue-500 focus:ring-blue-500 sm:text-sm"></textarea>
         </div>
         <div class="flex justify-end mt-4">
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded float-right">Submit</button>

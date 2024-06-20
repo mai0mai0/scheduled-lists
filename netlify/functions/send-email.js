5ee789dadd527e759b894994682f4f588ab4cc82
Aaron Mai <aaronmai@Aarons-Laptop.local>
2024-06-20 14:09:46 -0400

    new email testing again

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -29,7 +29,7 @@ exports.handler = async (event, context) => {
         // Create the email content
         let mailOptions = {
           from: process.env.EMAIL_USER,
-          to: 'ITHelpDesk@knak.com', 
+          to: 'ithelpdesk@knak.com', 
           subject: `Form Submission`,
           text: `Checks:\n\n${checkboxStatus}\n\nNotes: ${notes}`,
         };

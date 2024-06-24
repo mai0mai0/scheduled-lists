9a7d7527f746727ffdb391680a70b75eaffba18f
Aaron Mai <aaronmai21@gmail.com>
2024-06-24 12:44:18 -0400

    Page title will now match the name of the list fetched from JSON

--- app.vue
+++ app.vue
@@ -69,6 +69,11 @@ export default {
 </script>
 
 <template>
+  <div>
+    <Head>
+      <Title>{{ itemData.list_name }}</Title>
+    </Head>
+  </div>
   <div class="bg-white">
     <div class="bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
       <div v-if="itemData">
